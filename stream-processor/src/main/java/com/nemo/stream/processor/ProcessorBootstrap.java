eafd4d57949b5fcd321a761186d842cb2fcb3245
bixiyan <491676539@qq.com>
2017-11-27 23:37:43 +0800

    modify spring cloud stream

--- stream-processor/src/main/java/com/nemo/stream/processor/ProcessorBootstrap.java
+++ stream-processor/src/main/java/com/nemo/stream/processor/ProcessorBootstrap.java
@@ -3,9 +3,13 @@ package com.nemo.stream.processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Processor;
 import org.springframework.integration.annotation.ServiceActivator;
 
+@SpringBootApplication
+@EnableBinding(Processor.class)
 public class ProcessorBootstrap {
 	private static Logger logger = LoggerFactory.getLogger(ProcessorBootstrap.class);
 	private static String name = "logging";
--- stream-source/src/main/java/com/nemo/stream/output/SourceBootstrap.java
+++ stream-source/src/main/java/com/nemo/stream/output/SourceBootstrap.java
@@ -6,9 +6,13 @@ import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.integration.annotation.InboundChannelAdapter;
 
+@SpringBootApplication
+@EnableBinding(Source.class)
 public class SourceBootstrap {
 
 	private static Logger logger = LoggerFactory.getLogger(SourceBootstrap.class);
@@ -17,7 +21,7 @@ public class SourceBootstrap {
 		SpringApplication.run(SourceBootstrap.class, args);
 	}
 
-	@InboundChannelAdapter(value = Source.OUTPUT)
+	@InboundChannelAdapter(Source.OUTPUT)
 	public String timerMessageSource() {
 		String format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
 		logger.info("publish message :" + format);
